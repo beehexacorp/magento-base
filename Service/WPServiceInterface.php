e065689038985da60bc7388a02f0cedd77b0269d
Ubuntu <ubuntu@ubuntu.com>
2026-02-25 22:16:32 +0700

    2.4.6: Adding top menu

--- Service/WPServiceInterface.php
+++ Service/WPServiceInterface.php
@@ -17,5 +17,5 @@ interface WPServiceInterface
      * @param int $page
      * @return \Beehexa\Base\Service\Data\Wordpress\PostDataInterface[]
      */
-    public function getPosts(int $limit = 2, int $page = 1): array;
+    public function getPosts(int $limit = 2, int $page = 1);
 }
